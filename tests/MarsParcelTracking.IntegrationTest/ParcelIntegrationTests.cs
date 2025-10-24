cfcb4e4dacb6e00b225c690fc32aafbec039044c
moi <machinelearning001@meras.com.es>
2025-10-24 12:57:24 +0100

    rubbish

---
+++ tests/MarsParcelTracking.IntegrationTest/ParcelIntegrationTests.cs
@@ -0,0 +1,36 @@
+﻿using MarsParcelTracking.API;
+using MarsParcelTracking.API.Responses;
+using System.Net.Http.Json;
+
+namespace MarsParcelTracking.IntegrationTest
+{
+    public class ParcelIntegrationTests : IClassFixture<CustomWebApplicationFactory>
+    {
+        private readonly HttpClient _client;
+        public ParcelIntegrationTests(CustomWebApplicationFactory factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task RegisterParcel_ValidRequest_ReturnsSuccess()
+        {
+            var registerParcelRequest = new RegisterParcelRequest
+            {
+                Barcode = "RMARS1234567890123456789A",
+                Sender = "Cuco Malanga",
+                Recipient = "Conde Monte Cristo",
+                DeliveryService = "Standard",
+                Contents = "Signed C# language specification and a birthday card"
+            };
+
+            var response = await _client.PostAsJsonAsync("/api/parcels", registerParcelRequest);
+            response.EnsureSuccessStatusCode();
+
+            var result = await response.Content.ReadFromJsonAsync<GetParcelResponse>();
+
+            Assert.NotNull(result);
+            Assert.Equal(registerParcelRequest.Barcode, result.Barcode);
+        }
+    }
+}
---
+++ tests/MarsParcelTracking.IntegrationTest/Utilities.cs
@@ -0,0 +1,29 @@
+﻿using MarsParcelTracking.Domain;
+
+namespace MarsParcelTracking.IntegrationTest
+{
+    public static class Utilities
+    {
+        public static void InitializeDbForTests(ParcelContext db)
+        {
+            // Remove all existing data from ParcelTransitions table for a clean slate
+            db.ParcelTransitions.RemoveRange(db.ParcelTransitions);
+
+            // Remove all existing data from Parcels table to avoid duplicate key errors and stale data
+            db.Parcels.RemoveRange(db.Parcels);
+
+            // Persist all deletions to the in-memory database
+            db.SaveChanges();
+
+            {
+                var transition = new ParcelTransition { Id = 1, Status = EnumParcelStatus.Created, Timestamp = DateTime.UtcNow };
+                var parcel = new Parcel { Id = 1, Barcode = "asasasas" ,History= new List<ParcelTransition> { transition} };
+                transition.ParcelId = parcel.Id;
+
+                db.ParcelTransitions.AddRange(transition);
+                db.Parcels.AddRange(parcel);
+            }
+            db.SaveChanges();
+        }
+    }
+}
