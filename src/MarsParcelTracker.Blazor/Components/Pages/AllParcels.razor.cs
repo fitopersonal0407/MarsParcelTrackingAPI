836c24891079828f203144cf7a646135d779dc7a
moi <machinelearning001@meras.com.es>
2025-10-25 11:32:37 +0100

    AllParcels

--- src/MarsParcelTracker.Blazor/Components/Pages/AllParcels.razor.cs
+++ src/MarsParcelTracker.Blazor/Components/Pages/AllParcels.razor.cs
@@ -1,8 +1,51 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using MarsParcelTracking.API.Responses;
+using Microsoft.AspNetCore.Components;
 
 namespace MarsParcelTracker.Blazor.Components.Pages
 {
     public partial class AllParcels : ComponentBase
     {
+        [Inject]
+        protected IHttpClientFactory HttpClientFactory { get; set; } = null!;
+        protected HttpClient? _httpClient;
+
+        protected List<GetParcelResponse>? entities;
+        protected bool isLoading = false;
+        protected bool hasError = false;
+        protected string errorMessage = string.Empty;
+
+        protected override async Task OnInitializedAsync()
+        {
+            _httpClient = HttpClientFactory.CreateClient();
+            _httpClient.BaseAddress = new Uri("https://localhost:7168/");
+            _httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+
+            await LoadData();
+        }
+
+        protected async Task LoadData()
+        {
+            isLoading = true;
+            hasError = false;
+            errorMessage = string.Empty;
+            StateHasChanged();
+
+            try
+            {
+                var response = await _httpClient.GetAsync($"/api/parcels/");
+                entities = await response.Content.ReadFromJsonAsync<List<GetParcelResponse>>();
+            }
+            catch (Exception ex)
+            {
+                hasError = true;
+                errorMessage = ex.Message;
+                entities = new List<GetParcelResponse>();
+            }
+            finally
+            {
+                isLoading = false;
+                StateHasChanged();
+            }
+        }
     }
 }
