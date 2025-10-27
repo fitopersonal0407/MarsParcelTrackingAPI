abf56a1be4aa208d814674802d5c07bb2a2b88b1
moi <machinelearning001@meras.com.es>
2025-10-27 12:52:00 +0000

    adding services

--- src/MarsParcelTracker.Blazor.WebAssembly/Program.cs
+++ src/MarsParcelTracker.Blazor.WebAssembly/Program.cs
@@ -1,11 +1,15 @@
 using MarsParcelTracker.Blazor.WebAssembly;
+using MarsParcelTracker.Blazor.WebAssembly.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.ComponentModel.Design;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddSingleton<IParcelService, ParcelService>();
 
 await builder.Build().RunAsync();
