ea6301725bdb6d2c50c7ff6723f282cceb518c10
moi <machinelearning001@meras.com.es>
2025-10-27 17:55:09 +0000

    2025-10-27_007

---
+++ src/MarsParcelTracker.Blazor.WebAssembly/Services/HttpParcelService.cs
@@ -0,0 +1,32 @@
+﻿using MarsParcelTracker.Blazor.WebAssembly.Models;
+using System.Net.Http.Json;
+
+namespace MarsParcelTracker.Blazor.WebAssembly.Services
+{
+    public class HttpParcelService : IParcelService
+    {
+        private readonly IMessageService messageService;
+        private readonly HttpClient httpClient;
+        public HttpParcelService(IMessageService messageService, HttpClient httpClient)
+        {
+            this.messageService = messageService;
+            this.httpClient = httpClient;
+        }
+        public async Task<List<GetParcelResponse>> GetParcels()
+        {
+            messageService.Add(httpClient==null? "httpClient null" : "httpClient not null");
+
+            try
+            {
+                messageService.Add("HTTP ParcelService: fetched parcels");
+                return await httpClient.GetFromJsonAsync<List<GetParcelResponse>>("/api/parcels/");
+            }
+            catch (Exception ex)
+            {
+                messageService.Add(ex.Message);
+                throw;
+            }
+        }
+    }
+
+}
